d7632d27c696ac798d88e92a3d77fc62983a9e51
Mazhr <ahmedmazhar20@gmail.com>
2021-01-19 04:41:48 +0200

    Added Functions in ino

--- hardware/control/control.ino
+++ hardware/control/control.ino
@@ -4,9 +4,20 @@
   http://github.com/ax2mazhr
 +++++++++++++++++++
   To be used on arduino pro mini (Atmega328)
++++++++++++++++++++
+  Data Examples:
+    [3,5,6][255,255,255][1000] <-- Fade / PWM
+    [2,4][0,1] <-- On/Off
+    ==================
+    3,5,6;255,255,255;1000$
+    2,4;0,1;$
+
 */
 
-void setup() {
+int PWM3,PWM5,PWM6,PWM9,PWM10,PWM11 = 0;
+
+void setup() 
+{
   DDRD |= B11111100;    //Set pins 2-7 as OUTPUT
   PORTD &= B00000011;   //Set pins 2-7 to LOW
   DDRB |= B00111111;    //Set pins 8-13 as OUTPUT
@@ -16,10 +27,29 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+void loop() 
+{
   if(Serial.available())
   {
-    
+    parse(Serial.readStringUntil('$'));
   }
+}
 
+void parse(String input)
+{
+  String[] cmd = strchr(input,';');
+}
+
+void fade(int[] pins, int[] values, int duration)
+{
+  //map function
+  //timers
+}
+
+void switch(int[] pins, int[] values)
+{
+  for(int i=0 ; i<sizeof(pins); i++)
+  {
+    digitalWrite(pins[i],values[i]);
+  }
 }
